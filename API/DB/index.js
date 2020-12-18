25385ed08dca9bc8db0076b8bc17beddfe836521
hadiazb <hadiazb@unal.edu.co>
2020-12-18 15:31:25 -0500

    [ADD] Prettier, Eslint and .vscode

--- API/DB/index.js
+++ API/DB/index.js
@@ -4,18 +4,17 @@ const config = require('../config/index');
 const MONGODB_URI = `mongodb+srv://${config.database.dbUser}:${config.database.dbPassword}${config.database.dbHost}/${config.database.dbName}?retryWrites=true&w=majority`;
 
 module.exports = class MongoConnect {
-	constructor() {
+  constructor() {
     this.connect();
-	}
+  }
 
-	connect() {
-		mongoose
-			.connect(MONGODB_URI, {
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-			})
-			.then(() => console.log('Data base is connect!!'))
-			.catch((err) => console.log(err));
-	}
-
+  async connect() {
+    await mongoose
+      .connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log('Data base is connect!!'))
+      .catch((err) => console.log(err));
+  }
 };
--- API/server.js
+++ API/server.js
@@ -2,17 +2,14 @@ const express = require('express');
 const config = require('./config/index');
 const MongoConnect = require('./DB/index');
 
-const mongo = new MongoConnect();
+new MongoConnect();
 
-mongo.connect();
 const app = express();
 
-app.use('/', function (req, res) {
-	res.send('Hello world');
+app.use('/', (req, res) => {
+  res.send('Hello world');
 });
 
 app.listen(config.app.port, () => {
-	console.log(
-		`server running on http://localhost:${config.app.port}`
-	);
+  console.log(`server running on http://localhost:${config.app.port}`);
 });
