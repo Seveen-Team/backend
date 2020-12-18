5180ed87269f168f6b5c6e94a8e81e4abe2d9937
agustin <agustin.moranr@gmail.com>
2020-12-18 13:14:07 -0600

    [ADD] creación del modulo response.js dentro de utils, para gestionar las respuestas al cliente y los log de errores principales

---
+++ API/utils/response.js
@@ -0,0 +1,49 @@
+// This module help us to handle the responses we give to client and the logs that help us to debbug the code.
+// This way we give error and responses with more consitency
+
+const statusMessage = {
+  '200': 'Valid Request',
+  '201': 'Created',
+  '400': 'Invalid Format',
+  '500': 'Internal Error'  
+};
+
+//handle success request
+exports.success = function (req, res, data, status) {
+  let statusCode = status;
+  let result = data;
+
+  if(!status) {
+    statusCode = 200;
+  };
+
+  if(!data) {
+    result = statusMessage[statusCode];
+  };
+
+  res.status(statusCode).send({
+    error: '',
+    body: result
+  });
+};
+
+//handle success request
+exports.error = function (req, res, data, status, details) {
+  console.error('[Response Error]: ' + details)
+  let statusCode = status;
+  let result = data;
+
+  if(!status) {
+    statusCode = 500;
+  };
+
+  if(!data) {
+    result = statusMessage[statusCode];
+  };
+
+  res.status(statusCode).send({
+    error: result,
+    details: details,
+    body: ''
+  });
+};
