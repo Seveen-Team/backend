0570ba4674c6f03e0c77fb950ce4263821ad9e76
agustin <agustin.moranr@gmail.com>
2020-12-18 20:27:44 -0600

    [ADD] endpoint post para la creación de vacantes

--- API/components/vacant/controller.js
+++ API/components/vacant/controller.js
@@ -1,13 +1,33 @@
 const Vacants = require('./model');
 
 // Vacant list
-const list = () =>
+const getAll = () =>
   new Promise(async (resolve, reject) => {
     await Vacants.find({}, (error, vacants) =>
       error ? reject(`[Controller ERROR]: ${error}`) : resolve(vacants)
     );
+});
+
+const add = (vacantData) => {
+  return new Promise((resolve, reject) => {
+
+    //add default values
+    vacantData.modality = 'remote';
+    vacantData.userId = [];
+
+    //create vacant schema
+    let vacant = new Vacants(vacantData)
+    
+    //save on db
+    vacant.save((error, newVacant) => {
+      return error?
+      reject('[Controller ERROR: error on db save] ' + error)
+      :resolve(newVacant)
+    })
   });
+}
 
 module.exports = {
-  list,
+  getAll,
+  add,
 };
--- API/components/vacant/network.js
+++ API/components/vacant/network.js
@@ -4,13 +4,20 @@ const response = require('../../utils/response');
 
 const router = express.Router();
 
-list = async (req, res, next) => {
+router.get('/', list);
+router.post('/', create)
+
+async function list (req, res, next) {
   await vacantController
-    .list()
+    .getAll()
     .then((vacants) => response.success(req, res, vacants, 200))
     .catch((error) => response.error(req, res, 'Internal Error', 500, error));
 };
 
-router.get('/vacant', list);
+async function create (req, res, next) {
+  await vacantController.add(req.body)
+  .then((vacant) => response.success(req, res, vacant, 201))
+  .catch((error) => response.error(req, res, 'Internal Error', 500, error));
+}
 
 module.exports = router;
